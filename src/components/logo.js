27a7056b01e8d9a8407a6a2014e5cf0c10fe4871
Kait <fahrenbachkh@gmail.com>
2021-08-28 12:08:41 -0400

    modified logo size

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
  
 export default class Logo extends Component {
   render() {
+      const size = {
+          height: this.props.size? this.props.size : 105,
+          width: this.props.size ? this.props.size : 105
+      }
     return (
       <div className="logo-main">
-        <img alt="daily smart ui image logo big" src="/assets/ds_circle_logo.png"/>
+        <img style={size} alt="daily smart ui image logo big" src="/assets/ds_circle_logo.png"/>
       </div>
     );
   }
