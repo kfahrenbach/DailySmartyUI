a95c0c18fec3ccc34e3d06e4e69d2b01898d92a0
Kait <fahrenbachkh@gmail.com>
2021-08-28 15:00:02 -0400

    render result posts, set up render links

--- src/components/resultsPosts.js
+++ src/components/resultsPosts.js
@@ -5,11 +5,11 @@ import Post from './post'
 class ResultsPosts extends Component {
     renderPosts() {
         const posts = this.props.posts.map((post,index) => {
-            return <Post key={index} {...post} />
+            return <Post type="result" key={index} {...post} />
         })
         return posts;
     }
-    
+
     render() {
         return (
             <div className="results-posts">
